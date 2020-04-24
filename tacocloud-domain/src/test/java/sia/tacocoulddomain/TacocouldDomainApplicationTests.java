cd7e8d7a92f4ad69882b03b4a8a8efd36fd42a0f
Deyang Dai <deyangd@umich.edu>
2020-04-24 16:14:25 -0400

    init taco-cloud v0.0.6

---
+++ tacocloud-domain/src/test/java/sia/tacocoulddomain/TacocouldDomainApplicationTests.java
@@ -0,0 +1,13 @@
+package sia.tacocoulddomain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacocouldDomainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ tacocloud-ui/src/test/java/sia/tacocloudui/TacocloudUiApplicationTests.java
@@ -0,0 +1,13 @@
+package sia.tacocloudui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacocloudUiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
