074964f2da4e9b6c67837d3e1a08dea43d548718
Deyang Dai <deyangd@umich.edu>
2020-04-25 17:38:14 -0400

    add design-taco-api

---
+++ tacocloud-api/src/main/java/tacos/web/api/DesignTacoController.java
@@ -0,0 +1,56 @@
+package tacos.web.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.hateoas.server.EntityLinks;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import tacos.Taco;
+import tacos.data.TacoRepository;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path = "/design", produces = "application/json")
+@CrossOrigin(origins = "*")
+public class DesignTacoController {
+
+    private final TacoRepository tacoRepository;
+
+    private final EntityLinks entityLinks;
+
+    @Autowired
+    public DesignTacoController(TacoRepository tacoRepository, EntityLinks entityLinks) {
+        this.tacoRepository = tacoRepository;
+        this.entityLinks = entityLinks;
+    }
+
+    @GetMapping("/recent")
+    public Iterable<Taco> recentTacos() {
+        PageRequest pageRequest = PageRequest.of(0, 12, Sort.by("createdAt").descending());
+        return tacoRepository.findAll(pageRequest).getContent();
+    }
+
+    @PostMapping(consumes = "application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Taco postTaco(@RequestBody Taco taco) {
+        return tacoRepository.save(taco);
+    }
+
+//    @GetMapping("/{id}")
+//    public Taco tacoById(@PathVariable("id") Long id) {
+//        Optional<Taco> taco = tacoRepository.findById(id);
+//        return taco.orElse(null);
+//    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Taco> tacoById(@PathVariable("id") Long id) {
+        Optional<Taco> taco = tacoRepository.findById(id);
+        if (taco.isPresent()) {
+            return new ResponseEntity<>(taco.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    }
+}
